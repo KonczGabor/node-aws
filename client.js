8cc721ce01df96f6d7191c0f0b5ecdd4c4da58df
Gabor Koncz <gabor.koncz@nixs.com>
2024-02-22 15:39:40 +0100

    7

---
+++ client.js
@@ -0,0 +1,9 @@
+const { KMSClient } = require('@aws-sdk/client-kms');
+
+// Configure the AWS region of your KMS key
+const REGION = "eu-central-1"; // e.g., us-west-2
+
+// Create and configure an instance of the KMS client
+const kmsClient = new KMSClient({ region: REGION });
+
+module.exports = { kmsClient };
