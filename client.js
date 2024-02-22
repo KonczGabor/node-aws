39cf46fc4a5a1305551a3e16e4e13253728d6203
Gabor Koncz <gabor.koncz@nixs.com>
2024-02-22 19:04:00 +0100

    8

--- client.js
+++ client.js
@@ -4,6 +4,6 @@ const { KMSClient } = require('@aws-sdk/client-kms');
 const REGION = "eu-central-1"; // e.g., us-west-2
 
 // Create and configure an instance of the KMS client
-const kmsClient = new KMSClient({ region: REGION });
+const kmsClient = new KMSClient({   region: REGION });
 
 module.exports = { kmsClient };
--- decrypt.js
+++ decrypt.js
@@ -1,20 +1,17 @@
 const { DecryptCommand } = require('@aws-sdk/client-kms');
 const { kmsClient } = require('./kms-client');
 
-async function decryptData(ciphertextBlob) {
-    try {
-        // Cipher text should be in a Uint8Array or a base64-encoded string
-        const params = {
-            CiphertextBlob: Buffer.from(ciphertextBlob, 'base64'), // Convert from base64 to binary
-        };
-        const command = new DecryptCommand(params);
-        const response = await kmsClient.send(command);
+async function decryptData(encryptedData) {
+    const decryptParams = {
+        CiphertextBlob: encryptedData
+    };
 
-        // Plaintext is a Uint8Array, convert it to a string
-        const plaintext = new TextDecoder().decode(response.Plaintext);
-        return plaintext;
+    try {
+        const decryptCommand = new DecryptCommand(decryptParams);
+        const { Plaintext } = await kmsClient.send(decryptCommand);
+        return Plaintext.toString('base64');
     } catch (error) {
-        console.error("Error decrypting data:", error);
+        console.error('Error decrypting data:', error);
         throw error;
     }
 }
